c56f05db485593fb7e2d62b3c41114882f40d7e5
Donkelstein <31540328+Donkelstein@users.noreply.github.com>
2018-11-29 10:17:01 +0100

    Har gjort om sidan och använt det jag lärt mig föra lektionen.

--- index.php
+++ index.php
@@ -11,21 +11,30 @@
 	<link href="https://fonts.googleapis.com/css?family=Merriweather%7cMerriweather+Sans" rel="stylesheet"> 
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body style="background-image: url('Asset 2.svg'); background-repeat: no-repeat; background-size: 30%;">
-
-<ul class="nav justify-content-center" style="margin-top: 5vh;">
-  <li class="nav-item">
-    <a class="nav-link active h4" style="color: #3366FF" href="index.php">Hem</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link h4" style="color: #3366FF" href="play.php">Spela</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link h4" style="color: #3366FF" href="edit.php">Redigera</a>
-  </li>
-</ul>
+<body>
 
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="asset 2.svg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <img src="asset 2.svg" style="width: 12%; height: 12%; margin-left: 5vh">
+
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php" style="margin-left: 48vh;"><h3>Hem</h3><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="play.php" style="margin-left: 10vh;"><h3>Spela</h3></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="edit.php" style="margin-left: 10vh;"><h3>Redigera</h3></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 <?php
 
 ?>
--- play.php
+++ play.php
@@ -10,23 +10,32 @@
 	<title>Soloäventyr - Spela</title>
 	<link href="https://fonts.googleapis.com/css?family=Merriweather%7cMerriweather+Sans" rel="stylesheet"> 
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="story.css">
 </head>
-<body style="background-image: url('Asset 2.svg'); background-repeat: no-repeat;  background-size: 30%;">
+<body style="overflow: hidden;">
 
 <main>
-<ul class="nav justify-content-center" style="margin-top: 5vh;">
-  <li class="nav-item">
-    <a class="nav-link active h4" style="color: #3366FF" href="index.php">Hem</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link h4" style="color: #3366FF" href="play.php">Spela</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link h4" style="color: #3366FF" href="edit.php">Redigera</a>
-  </li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="asset 2.svg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <img src="asset 2.svg" style="width: 12%; height: 12%; margin-left: 5vh">
 
-
+      <li class="nav-item">
+        <a class="nav-link" href="index.php" style="margin-left: 48vh;"><h3>Hem</h3><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="play.php" style="margin-left: 10vh;"><h3>Spela</h3></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="edit.php" style="margin-left: 10vh;"><h3>Redigera</h3></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 
 
@@ -52,8 +61,8 @@
 		//prints out row, row being what you have written in the database at the given id number.
 		//echo "<pre>" . print_r($row,1) . "</pre>";
 
-		echo " <div class='row justify-content-center' style='margin-top:10vh;'>
-    			<div class='col-4'><blockquote class='blockquote text-center'>" . $row['text'] . "
+		echo " <div class='row justify-content-center' style='margin-top:25vh;'>
+    			<div class='col-4'><blockquote class='blockquote text-center style='style='font-size: 2em;'>" . $row['text'] . "
     			</blockquote>
     			</div> 
     			</div>";
@@ -69,7 +78,8 @@
 		echo "<div class='row justify-content-center'>
     			<div class='col-4'> <blockquote class='blockquote text-center'> ";
 		foreach ($row as $val) {
-			echo "<a class='userAnswer' style='margin-left: 10px; margin-right: 10px;' href=\"?page="  . $val['target'] . "\">" . $val['text'] ."</a>";
+			echo "<a class='userAnswer' style='font-size: 2em;'
+					href=\"?page="  . $val['target'] . "\">" . $val['text'] ."</a>";
 		}
 		echo "
 		</blockquote>
@@ -81,10 +91,10 @@
 		// use for returning player / cookie
 
 	} else {
-		echo "<div class='row justify-content-center' style='margin-top: 10vh;'>
+		echo "<div class='row justify-content-center' style='margin-top: 25vh;'>
     			<div class='col-4'>
 				<blockquote class='blockquote text-center'>
-    			<a href = '?page=1'> Spela </a>
+    			<a href = '?page=1' style='font-size: 6em; color: rgba(0,0,0,.5);'> Spela </a>
     			</blockquote>
     			</div>
     			</div>";
